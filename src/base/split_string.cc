6710abd5f800f3e09ac3b111ab20e85386fe5b93
aksnzhy <mctt90@gmail.com>
2017-06-10 21:08:21 +0800

    add split_string.h

---
+++ src/base/split_string.cc
@@ -0,0 +1,68 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2016 by contributors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//------------------------------------------------------------------------------
+
+/*
+Author: Chao Ma (mctt90@gmail.com)
+
+This file is the implementation of StringSplit utilities.
+*/
+
+#include "src/base/split_string.h"
+
+#include "src/base/common.h"
+
+// In most cases, delim contains only one character.  In this case, we
+// use CalculateReserveForVector to count the number of elements should
+// be reserved in result vector, and thus optimize SplitStringUsing.
+static int CalculateReserveForVector(const std::string& full, const char* delim) {
+  int count = 0;
+  if (delim[0] != '\0' && delim[1] == '\0') {
+    // Optimize the common case where delim is a single character.
+    char c = delim[0];
+    const char* p = full.data();
+    const char* end = p + full.size();
+    while (p != end) {
+      if (*p == c) {  // This could be optimized with hasless(v,1) trick.
+        ++p;
+      } else {
+        while (++p != end && *p != c) {
+          // Skip to the next occurence of the delimiter.
+        }
+        ++count;
+      }
+    }
+  }
+  return count;
+}
+
+void SplitStringUsing(const std::string& full,
+                      const char* delim,
+                      std::vector<std::string>* result) {
+  CHECK(delim != NULL);
+  CHECK(result != NULL);
+  result->reserve(CalculateReserveForVector(full, delim));
+  back_insert_iterator< std::vector<std::string> > it(*result);
+  SplitStringToIteratorUsing(full, delim, it);
+}
+
+void SplitStringToSetUsing(const std::string& full,
+                           const char* delim,
+                           std::set<std::string>* result) {
+  CHECK(delim != NULL);
+  CHECK(result != NULL);
+  simple_insert_iterator<std::set<std::string> > it(result);
+  SplitStringToIteratorUsing(full, delim, it);
+}
---
+++ src/base/split_string.h
@@ -0,0 +1,126 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2016 by contributors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//------------------------------------------------------------------------------
+
+/*
+Author: Chao Ma (mctt90@gmail.com)
+
+This file provides StringSplit utilities.
+*/
+
+#ifndef XLEARN_BASE_SPLIT_STRING_H_
+#define XLEARN_BASE_SPLIT_STRING_H_
+
+#include <set>
+#include <string>
+#include <vector>
+
+//------------------------------------------------------------------------------
+// Subdivide string |full| into substrings according to delimitors
+// given in |delim|.  |delim| should pointing to a string including
+// one or more characters.  Each character is considerred a possible
+// delimitor. For example:
+//
+//   vector<string> substrings;
+//   SplitStringUsing("apple orange\tbanana", "\t ", &substrings);
+//
+// results in three substrings:
+//
+//   substrings.size() == 3
+//   substrings[0] == "apple"
+//   substrings[1] == "orange"
+//   substrings[2] == "banana"
+//------------------------------------------------------------------------------
+
+void SplitStringUsing(const std::string& full,
+                      const char* delim,
+                      std::vector<std::string>* result);
+
+// This function has the same semnatic as SplitStringUsing.  Results
+// are saved in an STL set container.
+void SplitStringToSetUsing(const std::string& full,
+                           const char* delim,
+                           std::set<std::string>* result);
+
+template <typename T>
+struct simple_insert_iterator {
+  explicit simple_insert_iterator(T* t) : t_(t) { }
+
+  simple_insert_iterator<T>& operator=(const typename T::value_type& value) {
+    t_->insert(value);
+    return *this;
+  }
+
+  simple_insert_iterator<T>& operator*() { return *this; }
+  simple_insert_iterator<T>& operator++() { return *this; }
+  simple_insert_iterator<T>& operator++(int placeholder) { return *this; }
+
+  T* t_;
+};
+
+template <typename T>
+struct back_insert_iterator {
+  explicit back_insert_iterator(T& t) : t_(t) {}
+
+  back_insert_iterator<T>& operator=(const typename T::value_type& value) {
+    t_.push_back(value);
+    return *this;
+  }
+
+  back_insert_iterator<T>& operator*() { return *this; }
+  back_insert_iterator<T>& operator++() { return *this; }
+  back_insert_iterator<T> operator++(int placeholder) { return *this; }
+
+  T& t_;
+};
+
+template <typename StringType, typename ITR>
+static inline
+void SplitStringToIteratorUsing(const StringType& full,
+                                const char* delim,
+                                ITR& result) {
+  // Optimize the common case where delim is a single character.
+  if (delim[0] != '\0' && delim[1] == '\0') {
+    char c = delim[0];
+    const char* p = full.data();
+    const char* end = p + full.size();
+    while (p != end) {
+      if (*p == c) {
+        ++p;
+      } else {
+        const char* start = p;
+        while (++p != end && *p != c) {
+          // Skip to the next occurence of the delimiter.
+        }
+        *result++ = StringType(start, p - start);
+      }
+    }
+    return;
+  }
+
+  std::string::size_type begin_index, end_index;
+  begin_index = full.find_first_not_of(delim);
+  while (begin_index != std::string::npos) {
+    end_index = full.find_first_of(delim, begin_index);
+    if (end_index == std::string::npos) {
+      *result++ = full.substr(begin_index);
+      return;
+    }
+    *result++ = full.substr(begin_index, (end_index - begin_index));
+    begin_index = full.find_first_not_of(delim, end_index);
+  }
+}
+
+#endif // XLEARN_BASE_SPLIT_STRING_H_
