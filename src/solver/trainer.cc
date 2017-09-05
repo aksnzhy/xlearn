6ff2e5a33e758850083ac3618a08674f6b507914
aksnzhy <mctt90@gmail.com>
2017-09-04 18:07:02 -0700

    update

--- src/solver/trainer.cc
+++ src/solver/trainer.cc
@@ -87,7 +87,92 @@ void Trainer::Train() {
 
 // Training using cross-validation
 void Trainer::CVTrain() {
-
+  std::vector<real_t> train_loss(reader_list_.size());
+  std::vector<real_t> test_loss(reader_list_.size());
+  // Use ith Reader as validation Reader
+  for (int i = 0; i < reader_list_.size(); ++i) {
+    printf("  Cross-validation: %d/%lu: \n", i+1, reader_list_.size());
+    // for n epoch
+    for (int n = 0; n < epoch_; ++n) {
+      TIME_START();
+      // Use the other Reader to train
+      for (int j = 0; j < reader_list_.size(); ++j) {
+        if (i == j) { continue; }
+        train_reader_ = reader_list_[j];
+        train_reader_->Reset();
+        DMatrix* matrix = NULL;
+        while (train_reader_->Samples(matrix)) {
+          loss_->CalcGrad(matrix, model_, updater_);
+        }
+      }
+      // Calc train loss
+      index_t count_sample = 0;
+      real_t loss_val = 0.0;
+      std::vector<real_t> pred;
+      int tmp = 0;
+      for (int j = 0; j < reader_list_.size(); ++j) {
+        if (i == j) { continue; }
+        train_reader_ = reader_list_[j];
+        train_reader_->Reset();
+        DMatrix* matrix = NULL;
+        while (1) {
+          tmp = train_reader_->Samples(matrix);
+          if (tmp == 0) { break; }
+          if (tmp != pred.size()) {
+            pred.resize(tmp);
+          }
+          count_sample += tmp;
+          loss_->Predict(matrix, model_, pred);
+          loss_val += loss_->Evalute(pred, matrix->Y);
+        }
+      }
+      loss_val /= count_sample;
+      if (n == epoch_-1) {
+        train_loss[i] = loss_val;
+      }
+      printf("    Epoch %d  |  Train loss: %f  |", n, loss_val);
+      // Calc Test loss
+      count_sample = 0;
+      loss_val = 0.0;
+      tmp = 0;
+      test_reader_ = reader_list_[i];
+      test_reader_->Reset();
+      DMatrix* matrix = NULL;
+      while (1) {
+        tmp = test_reader_->Samples(matrix);
+        if (tmp == 0) { break; }
+        if (tmp != pred.size()) {
+          pred.resize(tmp);
+        }
+        count_sample += tmp;
+        loss_->Predict(matrix, model_, pred);
+        loss_val += loss_->Evalute(pred, matrix->Y);
+      }
+      loss_val /= count_sample;
+      if (n == epoch_-1) {
+        test_loss[i] = loss_val;
+      }
+      printf("  Test loss: %f  |", loss_val);
+      TIME_END();
+      printf("  ");
+      SHOW_TIME();
+      printf("  |\n");
+    }
+  }
+  // average train loss
+  real_t aver = 0;
+  for (int i = 0; i < train_loss.size(); ++i) {
+    aver += train_loss[i];
+  }
+  aver /= train_loss.size();
+  printf("  Average train loss: %f \n", aver);
+  // average test loss
+  aver = 0;
+  for (int i = 0; i < test_loss.size(); ++i) {
+    aver += test_loss[i];
+  }
+  aver /= test_loss.size();
+  printf("  Average test loss: %f \n", aver);
 }
 
 } // namespace xLearn
