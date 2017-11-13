7d43f2bab9349cb85bdb39b89e49579f38a1df6b
aksnzhy <mctt90@gmail.com>
2017-11-13 13:20:33 +0800

    update c_api_error.h

---
+++ src/c_api/c_api_error.h
@@ -0,0 +1,55 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2016 by contributors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//------------------------------------------------------------------------------
+
+/*
+Author: Chao Ma (mctt90@gmail.com)
+
+This file defines the error handling for C API. 
+*/
+
+#ifndef C_API_C_API_ERROR_H_
+#define C_API_C_API_ERROR_H_
+
+#include <string>
+
+#include "src/base/logging.h"
+#include "src/c_api/c_api.h"
+
+// Macro to guard begining and end section of all functions
+#define API_BEGIN() try {
+
+// Every function starts with API_BEGIN(); 
+// and finishes with API_END() or API_END_HANDLE_ERROR
+#define API_END() } catch(std::runtime_error &_except_) {           \
+  return XLAPIHandleException(_except_);                            \
+} return 0;  // NOLINT(*)
+
+// Every function starts with API_BEGIN(); 
+// and finishes with API_END() or API_END_HANDLE_ERROR
+// The finally clause contains procedure to cleanup states
+// when an error happens.
+#define API_END_HANDLE_ERROR(Finalize) } catch(std::runtime_error &_except_) { \
+  Finalize; return XLAPIHandleException(_except_); } return 0;  // NOLINT(*)
+
+// Handle exception thrown out and return value
+// of API after exception is handled
+inline int XLAPIHandleException(const std::runtime_error &e) {
+  LOG(FATAL) << "API runtime error: " 
+             << e.what();
+  return -1;
+}
+
+#endif  // C_API_C_API_ERROR_H_
