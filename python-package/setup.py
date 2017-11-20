20b7b65f912be9c9f64491f1dc8d7e0d91b4391d
aksnzhy <mctt90@gmail.com>
2017-11-20 20:19:55 +0800

    update python-package

--- python-package/setup.py
+++ python-package/setup.py
@@ -1,28 +1,30 @@
 """Setup xlearn package."""
+from __future__ import absolute_import
 import sys
 import os
-import shutil
-from setuptools import setup, Distribution, find_packages
+from setuptools import setup, find_packages
+sys.path.insert(0, '.')
 
-class BinaryDistribution(Distribution):
-  """Overrides Distribution class to bundle platform-specific binaries"""
-  # pylint: disable=R0201
-  def has_ext_modules(self):
-    """Has an extension module"""
-    return True
+CURRENT_DIR = os.path.dirname(__file__)
 
-LIBPATH_PY = os.path.abspath('./xlearn/libpath.py')
-LIBPATH = {'__file__': LIBPATH_PY}
-exec(compile(open(LIBPATH_PY, "rb").read(), LIBPATH_PY, 'exec'), LIBPATH, LIBPATH)
+libpath_py = os.path.join(CURRENT_DIR, 'xlearn/libpath.py')
+libpath = {'__file__': libpath_py}
+exec(compile(open(libpath_py, "rb").read(), libpath_py, 'exec'), libpath, libpath)
 
-# Path for C/C++ libraries
-LIB_PATH = LIBPATH['find_lib_path']()
+LIB_PATH = [os.path.relpath(libfile, CURRENT_DIR) for libfile in libpath['find_lib_path']()]
+print("Install libxlearn from: %s" % LIB_PATH)
 
 setup(name='xlearn',
-	  version=open(os.path.join(CURRENT_DIR, 'xlearn/VERSION')).read().strip(),
-	  description="xLearn Python Package",
+      version=open(os.path.join(CURRENT_DIR, 'xlearn/VERSION')).read().strip(),
+      description="xLearn Python Package",
       maintainer='Chao Ma',
       maintainer_email='mctt90@gmail.com',
+      zip_safe=False,
+      packages=find_packages(),
+      # this will use MANIFEST.in during install where we specify additional files,
+      # this is the golden line
+      include_package_data=True,
+      data_files=[('xlearn', LIB_PATH)],
       license='Apache-2.0',
       classifiers=['License :: OSI Approved :: Apache Software License'],
       url='https://github.com/aksnzhy/xlearn')
--- python-package/xlearn/libpath.py
+++ python-package/xlearn/libpath.py
@@ -1,43 +1,47 @@
 # coding: utf-8
 """Find the path to xlearn dynamic library files."""
+
 import os
 import platform
 import sys
 
 class XLearnLibraryNotFound(Exception):
-	"""Error thrown by when xlearn is not found"""
-	pass
+    """Error thrown by when xlearn is not found"""
+    pass
 
 def find_lib_path():
-	"""Find the path to xlearn dynamic library files.
+    """Find the path to xlearn dynamic library files.
 
-	Returns
-	-------
-	lib_path: list(string)
-	   List of all found library path to xlearn
-	"""
-	curr_path = os.path.dirname(op.path.abspath(os.path.expanduser(__file__)))
-	# make pythonpack hack: copy this directory one level upper for setup.py
-	dll_path = [curr_path, os.path.join(curr_path, '../../lib/'),
-	            os.path.join(curr_path, './lib/'),
-	            os.path.join(sys.prefix, 'xlearn')]
-	if sys.platform == 'win32':
-		if platform.architecture()[0] == '64bit':
-			dll_path.append(os.path.join(curr_path, '../../windowns/x64/Release/'))
-			# hack for pip installation when copy all parent source directory here
-			dll_path.append(os.path.join(curr_path, './windowns/x64/Release'))
-		else:
-			dll_path.append(os.path.join(curr_path, '../../windowns/Release'))
-			# hack for pip installation when copy all parent source directory here
-			dll_path.append(os.path.join(curr_path, './windowns/Release'))
-	elif sys.platform.startswith('linux'):
-		dll_path = [os.path.join(p, 'libxlearn.so') for p in dll_path]
+    Returns
+    -------
+    lib_path: list(string)
+       List of all found library path to xlearn
+    """
+    curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+    # make pythonpack hack: copy this directory one level upper for setup.py
+    dll_path = [curr_path, os.path.join(curr_path, '../../lib/'),
+                os.path.join(curr_path, './lib/'),
+                os.path.join(sys.prefix, 'xlearn')]
+    if sys.platform == 'win32':
+        if platform.architecture()[0] == '64bit':
+            dll_path.append(os.path.join(curr_path, '../../windows/x64/Release/'))
+            # hack for pip installation when copy all parent source directory here
+            dll_path.append(os.path.join(curr_path, './windows/x64/Release/'))
+        else:
+            dll_path.append(os.path.join(curr_path, '../../windows/Release/'))
+            # hack for pip installation when copy all parent source directory here
+            dll_path.append(os.path.join(curr_path, './windows/Release/'))
+        dll_path = [os.path.join(p, 'xlearn.dll') for p in dll_path]
+    elif sys.platform.startswith('linux'):
+        dll_path = [os.path.join(p, 'libxlearn.so') for p in dll_path]
     elif sys.platform == 'darwin':
-    	dll_path = [os.path.join(p, 'libxlearn.dylib') for p in dll_path]
+        dll_path = [os.path.join(p, 'libxlearn.dylib') for p in dll_path]
 
     lib_path = [p for p in dll_path if os.path.exists(p) and os.path.isfile(p)]
 
+    # From github issues, most of installation errors come from machines w/o compilers
     if not lib_path:
-    	raise XLearnLibraryNotFound(
-    		'Cannot find xLearn library in the condidate path.')
+        raise XLearnLibraryNotFound(
+            'Cannot find xlearn Library in the candidate path'
+            )
     return lib_path
