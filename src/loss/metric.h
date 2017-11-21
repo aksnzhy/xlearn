eca88dc48091a1591938db6ee6011cee127c2e83
dataflow <dataflow>
2017-11-21 09:45:42 +0800

    update: metric.h

--- src/loss/metric.h
+++ src/loss/metric.h
@@ -452,8 +452,6 @@ class AUCMetric : public Metric {
  public:
   struct Info {
     Info() {
-      positive_vec_.clear();
-      negative_vec_.clear();
       positive_vec_.resize(kMaxBucketSize, 0);
       negative_vec_.resize(kMaxBucketSize, 0);
     }
