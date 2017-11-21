5a2a4109ad5d16b65f4d7c198f3fddc3cf77eb8d
aksnzhy <mctt90@gmail.com>
2017-11-21 16:30:07 +0800

    update install-python.sh

--- python-package/test_python.py
+++ python-package/test_python.py
@@ -1,3 +1,6 @@
+# This file test the xlearn python package.
+# We create a ffm model for binary classification problem.
+# The dataset comes from the criteo CTR  .
 import xlearn as xl
 
 ffm_model = fm.create_ffm()
