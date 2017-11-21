6a571a6fc3c1aa0b8cd01ebc2ca254eab795d32a
aksnzhy <mctt90@gmail.com>
2017-11-21 16:33:16 +0800

    add README.md

--- python-package/test_python.py
+++ python-package/test_python.py
@@ -1,8 +1,8 @@
 # This file test the xlearn python package.
 # We create a ffm model for binary classification problem.
-# The dataset comes from the criteo CTR  .
+# The dataset comes from the criteo CTR.
 import xlearn as xl
 
-ffm_model = fm.create_ffm()
+ffm_model = xl.create_ffm()
 ffm_model.serTrain(..)
 ffm_model.setValidate(..)
