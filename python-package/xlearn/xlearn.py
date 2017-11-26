15f1fc6683771cd7d4b61c8b191043b40b752b8b
Yi Tang <yi.tang.uk@me.com>
2017-11-26 22:44:47 +0000

    fix typoes

--- python-package/xlearn/xlearn.py
+++ python-package/xlearn/xlearn.py
@@ -52,11 +52,11 @@ class XLearn(object):
 				_check_call(_LIB.XLearnSetFloat(ctypes.byref(self.handle), 
 					c_str(key), ctypes.c_float(value)))
 			elif key == 'epoch':
-				_check_call(_LIB(XLearnSetInt(ctypes.byref(self.handle), 
-					c_str(key), ctypes.c_uint(value))))
+				_check_call(_LIB.XLearnSetInt(ctypes.byref(self.handle), 
+					c_str(key), ctypes.c_uint(value)))
 			elif key == 'fold':
-				_check_call(_LIB(XLearnSetInt(ctypes.byref(self.handle), 
-					c_str(key), ctypes.c_uint(value))))
+				_check_call(_LIB.XLearnSetInt(ctypes.byref(self.handle), 
+					c_str(key), ctypes.c_uint(value)))
 			else:
 				raise Exception("Invalid key!", key)
 
