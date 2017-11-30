fd55168171474413811ba407147e23c1735f920a
dataflow <dataflow>
2017-11-30 21:14:35 +0800

    rm

--- src/data/model_parameters.h
+++ src/data/model_parameters.h
@@ -151,9 +151,6 @@ class Model {
   // Get the number of k.
   inline index_t GetNumK() { return num_K_; }
 
-  // Get the auxiliary cache size.
-  inline index_t GetAuxilarySize() { return auxiliary_size_; }
-
   // Get the aligned size of K.
   inline index_t get_aligned_k() {
     return (index_t)ceil((real_t)num_K_/kAlign)*kAlign;
