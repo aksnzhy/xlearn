1fde48a4892877e51048c2f343e3955b1f539130
aksnzhy <mctt90@gmail.com>
2017-11-30 22:44:45 +0800

    update ftrl parameter

--- src/data/hyper_parameters.h
+++ src/data/hyper_parameters.h
@@ -73,6 +73,11 @@ struct HyperParam {
   real_t regu_lambda = 0.00002;
   /* Hyper param for init model parameters */
   real_t model_scale = 0.66;
+  /* Used for ftrl */
+  real_t alpha = 5e-2;
+  real_t beta = 1.0;
+  real_t lambda_1 = 5e-5;
+  real_t lambda_2 = 15.0;
   /* Number of epoch. 
   This value could be changed in early-stop */
   int num_epoch = 10;
