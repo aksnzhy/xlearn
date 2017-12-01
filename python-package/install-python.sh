17b7fb9d900fec783add0ad18e4b3d35f0f80038
peikai zheng <bnuzpk@gmail.com>
2017-12-02 01:40:16 +0800

    fix python install bugs

--- python-package/install-python.sh
+++ python-package/install-python.sh
@@ -2,6 +2,3 @@
 # This script is for installization of xlearn python package
 # You may need to type your password here
 sudo python setup.py install
-# Reset the path of python library
-basepath=$(cd `dirname $0`; pwd)
-export PYTHONPATH=${basepath}/xlearn
