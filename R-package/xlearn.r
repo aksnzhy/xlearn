9aee126d26d5f297cc11a9202dc3fbf2306d1ca4
xswang <2012wxs@gmail.com>
2017-12-03 13:13:50 +0800

    update

--- R-package/xlearn.r
+++ R-package/xlearn.r
@@ -1,11 +1,9 @@
 dyn.load("/Users/xiaoshuwang/documents/oneflow/code/rl_api.so")
 
 test <- function() {
-  a <- 'Start and end with single quote'
+  a <- 'hello xlearn, I am R'
   print(a)
 }
 
 showinfo <- function () {
-  XLearn* xl = new XLearn;
-  xl->GetHyperParam().score_func
 }
