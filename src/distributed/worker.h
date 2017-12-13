41c036a59319e298a00bc7b740eb1cd8ac117c0e
xswang <2012wxs@gmail.com>
2017-12-13 20:48:22 +0800

    add: worker.h

---
+++ src/distributed/worker.h
@@ -0,0 +1,24 @@
+/*
+ * worker.h
+ * Copyright (C) 2017 wangxiaoshu <2012wxs@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef WORKER_H
+#define WORKER_H
+
+class XLearnWorker {
+ public:
+  XLearnWorker() {
+    kv_w_ = new ps::KVWorker<float>(0);
+    kv_v_ = new ps::KVWorker<float>(1);
+  }
+  ~XLearnWorker() {}
+
+ private:
+  ps::KVWorker<float>* kv_w_;
+  ps::KVWorker<float>* kv_v_;
+};
+
+#endif /* !WORKER_H */
