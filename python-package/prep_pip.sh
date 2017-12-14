8df876c106d861891f7b6367522d47eb8e3c481e
peikai zheng <bnuzpk@gmail.com>
2017-12-15 01:25:56 +0800

    code style

--- python-package/prep_pip.sh
+++ python-package/prep_pip.sh
@@ -4,7 +4,7 @@ cp setup_pip.py setup.py
 python setup.py sdist
 
 # for testpypi
-#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 # make sure you know what you gonna do, and uncomment the following line
 # twine upload dist/*.tar.gz
