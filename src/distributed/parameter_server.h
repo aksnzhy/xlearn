b137064396533524f907b35a30e5460ca271fa73
aksnzhy <mctt90@gmail.com>
2018-01-01 21:55:42 +0800

    update KVStore

--- src/distributed/parameter_server.h
+++ src/distributed/parameter_server.h
@@ -13,3 +13,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //------------------------------------------------------------------------------
+
+/*
+Author: Chao Ma (mctt90@gmail.com)
+
+This file defines the KVStore class, which allows workers to get
+and set the model parameters.
+*/
+
+#ifndef XLEARN_DISTRIBUTED_KVSTORE_H_
+#define XLEARN_DISTRIBUTED_KVSTORE_H_
+
+#include <vector>
+
+#include "src/base/common.h"
+
+namespace xLearn {
+
+//------------------------------------------------------------------------------
+// KVStore are used for distributed training and it allows workers to get
+// and set the model parameters by using pull() and push() API.
+//------------------------------------------------------------------------------
+class KVStore {
+ public:
+   // Constructor and Destructor
+   KVStore() { }
+   ~KVStore() { }
+
+   // Push a list of (feature id, value) into store
+   virtual void Push(const std::vector<index_t>& feat_list,
+   	                 const std::vector<real_t>& value_list);
+
+   // Pull the values for a list of feature ids
+   virtual void Pull(const std::vector<index_t>& feat_list,
+   	                 const std::vector<real_t>* value_list);
+};
+
+}  // namespace xLearn
+
+#endif  // XLEARN_DISTRIBUTED_KVSTORE_H_
