75200be0b64801831cfac6e3a976db5e492e7825
aksnzhy <mctt90@gmail.com>
2018-01-02 13:59:13 +0800

    update distributed training

--- src/distributed/parameter_server.cc
+++ src/distributed/parameter_server.cc
@@ -88,7 +88,7 @@ void KVStore::Pull(const std::vector<index_t>& key,
 // | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
 //  ---------------------------------------
 //   |   |   |   |   |   |   |   |   |   |
-//  s1  s2  s3  s4  s5  s6  s7  s8  s9  s10
+//  s1  s2  s3  s1  s2  s3  s1  s2  s3  s1
 //
 // On each local server:
 //
