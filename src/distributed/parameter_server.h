e80213a62e9d74c11c78f546c62eb195019dbddb
aksnzhy <mctt90@gmail.com>
2018-01-03 13:15:04 +0800

    update distributed training

--- src/distributed/parameter_server.h
+++ src/distributed/parameter_server.h
@@ -88,8 +88,9 @@ class KVStore {
    virtual void Pull(const std::vector<index_t>& key,
    	                 std::vector<real_t>* value_list,
    	                 const size_t length);
-   
+
  protected:
+   //---------------------------------------------------------------------------
    // In xLearn, we use a simple range strategy for model partiton
    // on parameter server. For example, we have 10 features and 3 
    // server nodes.
@@ -98,7 +99,7 @@ class KVStore {
    // | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
    //  ---------------------------------------
    //   |   |   |   |   |   |   |   |   |   |
-   //  s1  s2  s3  s4  s5  s6  s7  s8  s9  s10
+   //  s1  s2  s3  s1  s2  s3  s1  s2  s3  s1
    //
    // On each local server:
    //
@@ -108,11 +109,12 @@ class KVStore {
    //  ---------------      -----------      -----------
    //   |   |   |   |         |  |   |        |   |   |
    //   0   3   6   9         1  4   7        2   5   8
+   //---------------------------------------------------------------------------
 
-   // Given a feature id, return the server id, which stores that feature.
+   // Given a feature id, return the server id
    virtual size_t GetServerId(const index_t feat_id) const;
 
-   // Mapping the global feature id to the local server id.
+   // Mapping the global feature id to a local feature id
    virtual index_t Map(const index_t feat_id) const;
 };
 
