e518f293af507efa12f43c432c31a7505475ccd2
aksnzhy <mctt90@gmail.com>
2018-03-06 22:39:15 -0800

    fix base

--- src/base/thread_pool.h
+++ src/base/thread_pool.h
@@ -73,7 +73,6 @@ private:
     std::vector<std::thread> workers;
     // the task queue
     std::queue<std::function<void()>> tasks;
-
     // synchronization
     std::mutex queue_mutex;
     std::condition_variable condition;
@@ -81,7 +80,7 @@ private:
     std::atomic_int sync { 0 };
 };
 
-// the constructor just launches some amount of workers
+// The constructor just launches some amount of workers
 inline ThreadPool::ThreadPool(size_t threads)
     : stop(false) {
   for(size_t i = 0; i<threads; ++i)
@@ -94,8 +93,9 @@ inline ThreadPool::ThreadPool(size_t threads)
               std::unique_lock<std::mutex> lock(this->queue_mutex);
               this->condition.wait(lock,
                 [this]{ return this->stop || !this->tasks.empty(); });
-              if(this->stop && this->tasks.empty())
+              if (this->stop && this->tasks.empty()) {
                 return;
+              }
               task = std::move(this->tasks.front());
               this->tasks.pop();
             }
@@ -106,29 +106,25 @@ inline ThreadPool::ThreadPool(size_t threads)
   );
 }
 
-// add new work item to the pool
+// Add new work item to the pool
 template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args)
-    -> std::future<typename std::result_of<F(Args...)>::type>
-{
-    using return_type = typename std::result_of<F(Args...)>::type;
-
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-
-    std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-
-        // don't allow enqueueing after stopping the pool
-        if(stop)
-            throw std::runtime_error("enqueue on stopped ThreadPool");
-
-        tasks.emplace([task](){ (*task)(); });
+    -> std::future<typename std::result_of<F(Args...)>::type> {
+  using return_type = typename std::result_of<F(Args...)>::type;
+  auto task = std::make_shared< std::packaged_task<return_type()> >(
+    std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+  );
+  std::future<return_type> res = task->get_future();
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    // don't allow enqueueing after stopping the pool
+    if (stop) {
+      throw std::runtime_error("enqueue on stopped ThreadPool");
     }
-    condition.notify_one();
-    return res;
+    tasks.emplace([task](){ (*task)(); });
+  }
+  condition.notify_one();
+  return res;
 }
 
 // Wait all thread to finish their jobs
@@ -143,15 +139,15 @@ inline size_t ThreadPool::ThreadNumber() {
 }
 
 // the destructor joins all threads
-inline ThreadPool::~ThreadPool()
-{
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        stop = true;
-    }
-    condition.notify_all();
-    for(std::thread &worker: workers)
-        worker.join();
+inline ThreadPool::~ThreadPool() {
+  {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    stop = true;
+  }
+  condition.notify_all();
+  for (std::thread &worker: workers) {
+    worker.join();
+  }
 }
 
 // Get start and end index used in multi-thread training
