69c35442bcd2b6d4d9eaace03cee55b307872ace
askeySnip <296181278@qq.com>
2018-07-14 12:45:28 +0800

    travis-ci

--- build.sh
+++ build.sh
@@ -9,5 +9,9 @@ make
 
 cd python-package
 if command -v python2; then
-    sudo python2 setup.py install
+    python2 setup.py install
+fi
+
+if command -v python3; then
+    python3 setup.py install
 fi
