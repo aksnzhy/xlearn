32d1b5acc6634dc8d023ed275bc4fb141ccd0840
aksnzhy <mctt90@gmail.com>
2018-08-05 22:29:35 +0800

    update license

--- src/loss/loss.h
+++ src/loss/loss.h
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// Copyright (c) 2016 by contributors. All Rights Reserved.
+// Copyright (c) 2018 by contributors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
