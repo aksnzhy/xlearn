2e230f27e5bdf00ea6f0ccd5ac58fe0245104cce
aksnzhy <mctt90@gmail.com>
2018-09-15 19:11:49 +0800

    update math.h

--- src/base/math.h
+++ src/base/math.h
@@ -169,7 +169,7 @@ static inline real_t fastersigmoid(real_t x) {
 static inline real_t InvSqrt(real_t x) {
   real_t xhalf = 0.5f*x;
   int i = *reinterpret_cast<int*>(&x);  // get bits for floating VALUE
-  i = 0x5f375a86- (i>>1);  // gives initial guess y0
+  i = 0x5f375a86-(i>>1);  // gives initial guess y0
   x = *reinterpret_cast<real_t*>(&i);  // convert bits BACK to float
   x = x*(1.5f-xhalf*x*x);  // Newton step, repeating increases accuracy
   return x;
