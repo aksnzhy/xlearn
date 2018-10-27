1a86033512e8b80acf66a5bff560e5bfd363f406
aksnzhy <mctt90@gmail.com>
2018-10-27 23:01:12 +0800

    incremental reader

--- src/reader/reader.h
+++ src/reader/reader.h
@@ -151,6 +151,7 @@ class Reader {
 // For in-memory smaplling, the Reader will automatically convert
 // txt data to binary data, and uses this binary data in the next time.
 //------------------------------------------------------------------------------
+// TODO(aksnzhy): implement incremental reader
 class InmemReader : public Reader {
  public:
   // Constructor and Destructor
