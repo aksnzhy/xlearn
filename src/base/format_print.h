1a9caf03eb2115122aba724a961aea3076275c32
etVERITAS <bravewavy@gmail.com>
2019-02-01 20:42:19 +0800

    fix console text setting on windows

--- src/base/format_print.h
+++ src/base/format_print.h
@@ -24,6 +24,11 @@ This file defines facilities for format printing.
 #include <iostream>
 #include <vector>
 #include <string>
+#ifdef _MSC_VER
+#define NOMINMAX
+#include <windows.h>
+#endif
+
 
 #include "src/base/common.h"
 
@@ -32,40 +37,81 @@ namespace Color {
 #define IMPORTANT_MSG true
 #define NOT_IMPORTANT_MSG false
 
+#ifdef _MSC_VER
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+#endif
+
+
+
 // Color table
 enum Code {
+#ifndef _MSC_VER
 /*********************************************************
  *  Color for front                                      *
  *********************************************************/
-  FG_RED      = 31,
-  FG_GREEN    = 32,
-  FG_YELLOW   = 33,
-  FG_BLUE     = 34,
-  FG_MAGENTA  = 35,
-  FG_CYAN     = 36,
-  FG_WHITE    = 37,
-  FG_DEFAULT  = 39,
+    FG_RED      = 31,
+    FG_GREEN    = 32,
+    FG_YELLOW   = 33,
+    FG_BLUE     = 34,
+    FG_MAGENTA  = 35,
+    FG_CYAN     = 36,
+    FG_WHITE    = 37,
+    FG_DEFAULT  = 39,
 /*********************************************************
  *  Color for Background                                 *
  *********************************************************/
-  BG_RED      = 41,
-  BG_GREEN    = 42,
-  BG_YELLOW   = 43,
-  BG_BLUE     = 44,
-  BG_MAGENTA  = 45,
-  BG_CYAN     = 46,
-  BG_WHITE    = 47,
-  BG_DEFAULT  = 49,
+    BG_RED      = 41,
+    BG_GREEN    = 42,
+    BG_YELLOW   = 43,
+    BG_BLUE     = 44,
+    BG_MAGENTA  = 45,
+    BG_CYAN     = 46,
+    BG_WHITE    = 47,
+    BG_DEFAULT  = 49,
 /*********************************************************
  *  Control code                                         *
  *********************************************************/  
-  RESET          = 0,   // everything back to normal
-  BOLD           = 1,   // often a brighter shade of the same colour
-  UNDER_LINE     = 4,   
-  INVERSE        = 7,   // swap forground and background color
-  BOLD_OFF       = 21,
-  UNDER_LINE_OFF = 24,
-  INVERSE_OFF    = 27
+    RESET          = 0,   // everything back to normal
+    BOLD           = 1,   // often a brighter shade of the same colour
+    UNDER_LINE     = 4,   
+    INVERSE        = 7,   // swap forground and background color
+    BOLD_OFF       = 21,
+    UNDER_LINE_OFF = 24,
+    INVERSE_OFF    = 27
+#else
+/*********************************************************
+*  Color for front                                      *
+*********************************************************/
+	FG_RED = FOREGROUND_RED,
+	FG_GREEN = FOREGROUND_GREEN,
+	FG_YELLOW = FOREGROUND_RED | FOREGROUND_GREEN,
+	FG_BLUE = FOREGROUND_BLUE,
+	FG_MAGENTA = FOREGROUND_BLUE | FOREGROUND_RED,
+	FG_CYAN = FOREGROUND_BLUE | FOREGROUND_GREEN,
+	FG_WHITE = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+	FG_DEFAULT = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+/*********************************************************
+*  Color for Background                                 *
+*********************************************************/
+	BG_RED = BACKGROUND_RED,
+	BG_GREEN = BACKGROUND_GREEN,
+	BG_YELLOW = BACKGROUND_RED | BACKGROUND_GREEN,
+	BG_BLUE = BACKGROUND_BLUE,
+	BG_MAGENTA = BACKGROUND_BLUE | BACKGROUND_RED,
+	BG_CYAN = BACKGROUND_BLUE | BACKGROUND_GREEN,
+	BG_WHITE = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE,
+	BG_DEFAULT = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE,
+/*********************************************************
+*  Control code                                         *
+*********************************************************/
+	RESET = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,  // everything back to normal
+	BOLD = FOREGROUND_INTENSITY,   // often a brighter shade of the same colour
+	UNDER_LINE = COMMON_LVB_UNDERSCORE,
+	INVERSE = COMMON_LVB_REVERSE_VIDEO,   // swap forground and background color
+	BOLD_OFF = 0,
+	UNDER_LINE_OFF = 0,
+	INVERSE_OFF = 0
+#endif
 };
 
 //------------------------------------------------------------------------------
@@ -77,53 +123,86 @@ enum Code {
 //   cout << "This ->" << red << "word" << def "<- is red. " << endl;
 //------------------------------------------------------------------------------
 class Modifier {
-  Code code;
- public:
-  Modifier(Code pCode) : code(pCode) {}
-  friend std::ostream&
-  operator<<(std::ostream& os, const Modifier& mod) {
-  	return os << "\033[" << mod.code << "m";
-  }
- private:
-  DISALLOW_COPY_AND_ASSIGN(Modifier);
+    Code code;
+public:
+    Modifier(Code pCode) : code(pCode) { }
+#ifndef _MSC_VER
+    friend std::ostream &operator<<(std::ostream &os, const Modifier &mod) {
+        return os << "\033[" << mod.code << "m";
+    }
+#else
+	WORD GetCode() const { return code;  }
+#endif
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(Modifier);
 };
 
 // [Warning] blablabla ...
-inline void print_warning(const std::string& out) {
-  Color::Modifier mag(Color::FG_MAGENTA);
-  Color::Modifier bold(Color::BOLD);
-  Color::Modifier reset(Color::RESET);
-  std::cout << mag << bold << "[ WARNING    ] " 
-            << out << reset << std::endl;
+inline void print_warning(const std::string &out) {
+    Color::Modifier mag(Color::FG_MAGENTA);
+    Color::Modifier bold(Color::BOLD);
+    Color::Modifier reset(Color::RESET);
+#ifndef _MSC_VER
+    std::cout << mag << bold << "[ WARNING    ] "
+              << out << reset << std::endl;
+#else
+	SetConsoleTextAttribute(hConsole, mag.GetCode() | bold.GetCode());
+	std::cout << "[ WARNING    ] " << out << std::endl;
+	SetConsoleTextAttribute(hConsole, reset.GetCode());
+#endif
 }
 
-inline void print_error(const std::string& out) {
-  Color::Modifier red(Color::FG_RED);
-  Color::Modifier bold(Color::BOLD);
-  Color::Modifier reset(Color::RESET);
-  std::cout << red << bold << "[ ERROR      ] "
-            << out << reset << std::endl;
+inline void print_error(const std::string &out) {
+    Color::Modifier red(Color::FG_RED);
+    Color::Modifier bold(Color::BOLD);
+    Color::Modifier reset(Color::RESET);
+#ifndef _MSC_VER
+	std::cout << red << bold << "[ ERROR      ] "
+			  << out << reset << std::endl;
+#else
+	SetConsoleTextAttribute(hConsole, red.GetCode() | bold.GetCode());
+	std::cout << "[ ERROR      ] " << out << std::endl;
+	SetConsoleTextAttribute(hConsole, reset.GetCode());
+#endif
 }
 
-inline void print_action(const std::string& out) {
-  Color::Modifier green(Color::FG_GREEN);
-  Color::Modifier bold(Color::BOLD);
-  Color::Modifier reset(Color::RESET);
-  std::cout << green << bold << "[ ACTION     ] "
-            << out << reset << std::endl;
+inline void print_action(const std::string &out) {
+    Color::Modifier green(Color::FG_GREEN);
+    Color::Modifier bold(Color::BOLD);
+    Color::Modifier reset(Color::RESET);
+#ifndef _MSC_VER
+    std::cout << green << bold << "[ ACTION     ] "
+              << out << reset << std::endl;
+#else
+	SetConsoleTextAttribute(hConsole, green.GetCode() | bold.GetCode());
+	std::cout << "[ ACTION     ] " << out << std::endl;
+	SetConsoleTextAttribute(hConsole, reset.GetCode());
+#endif
 }
 
-inline void print_info(const std::string& out, bool important = false) {
-  Color::Modifier green(Color::FG_GREEN);
-  Color::Modifier bold(Color::BOLD);
-  Color::Modifier reset(Color::RESET);
-  if (!important) {
-    std::cout << green << "[------------] " << reset
-            << out << std::endl;
-  } else {
-    std::cout << green << bold << "[------------] " << out << reset
-              << std::endl;
-  }
+inline void print_info(const std::string &out, bool important = false) {
+    Color::Modifier green(Color::FG_GREEN);
+    Color::Modifier bold(Color::BOLD);
+    Color::Modifier reset(Color::RESET);
+    if (!important) {
+#ifndef _MSC_VER
+		std::cout << green << "[------------] " << reset << out << std::endl;
+#else
+		SetConsoleTextAttribute(hConsole, green.GetCode());
+		std::cout << "[------------] ";
+		SetConsoleTextAttribute(hConsole, reset.GetCode());
+		std::cout << out << std::endl;
+#endif
+    } else {
+#ifndef _MSC_VER
+        std::cout << green << bold << "[------------] " << out << reset << std::endl;
+#else
+		SetConsoleTextAttribute(hConsole, green.GetCode() | bold.GetCode());
+		std::cout << "[------------] " << out << std::endl;
+		SetConsoleTextAttribute(hConsole, reset.GetCode());
+#endif
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -140,16 +219,15 @@ inline void print_info(const std::string& out, bool important = false) {
 //   Meat       0x104       30    10.13
 //------------------------------------------------------------------------------
 template <typename T>
-void print_row(const std::vector<T>& column, 
-               const std::vector<int>& width) {
-  CHECK_EQ(column.size(), width.size());
-  for (size_t i = 0; i < column.size(); ++i) {
-    std::cout.width(width[i]);
-    std::cout << column[i];
-  }
-  std::cout << "\n";
+void print_now(const std::vector<T> &column, const std::vector<int> &width) {
+    CHECK_EQ(column.size(), width.size());
+    for (size_t i = 0; i < column.size(); ++i) {
+        std::cout.width(width[i]);
+        std::cout << column[i];
+    }
+    std::cout << "\n";
 }
 
-}  // namespace Color
+} // namespace Color
 
-#endif  // XLEARN_BASE_FORMAT_PRINT_H_
+#endif // XLEARN_BASE_FORMAT_PRINT_H_
