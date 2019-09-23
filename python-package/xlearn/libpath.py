383cfdaa5276d20f9d091224179af2b0a431f410
Lean-Y <yueling.me@qq.com>
2019-09-23 21:48:21 +0800

    Added a search path in the build directory

--- python-package/xlearn/libpath.py
+++ python-package/xlearn/libpath.py
@@ -34,6 +34,7 @@ def find_lib_path():
     curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
     # make pythonpack hack: copy this directory one level upper for setup.py
     dll_path = [curr_path, os.path.join(curr_path, '../../lib/'),
+                os.path.join(curr_path, '../../build/lib/'),
                 os.path.join(curr_path, './lib/'),
                 os.path.join(sys.prefix, 'xlearn')]
     if sys.platform == 'win32':
