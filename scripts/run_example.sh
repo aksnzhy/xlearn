480b85add36f5fa693443c063cc042f2b4f8aea0
aksnzhy <mctt90@gmail.com>
2019-09-24 09:07:41 +0800

    remove sudo for example

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -13,6 +13,6 @@
 # limitations under the License.
 
 # Training task
-sudo ./xlearn_train ./small_train.txt -s 2 -v ./small_test.txt -x acc
+./xlearn_train ./small_train.txt -s 2 -v ./small_test.txt -x acc
 # Prediction task
-sudo ./xlearn_predict ./small_test.txt ./small_train.txt.model --sigmoid
+./xlearn_predict ./small_test.txt ./small_train.txt.model --sigmoid
